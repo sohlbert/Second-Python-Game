eff8b3f618149bb687b26e28b8dac32528ece621
Robert Sohlberg <45578298+sohlbert@users.noreply.github.com>
2019-10-02 20:49:30 -0500

    Add files via upload

--- Snake.Game.py
+++ Snake.Game.py
@@ -1,10 +1,21 @@
 #Making Snake in Pygame
+ 
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
 
 class cube(object):
-    rows = 0
-    w = 0
+    rows = 20
+    w = 500
     def __init___(self, start, dirnx=1, dirny=0, color=(255,0,0)):
-        pass
+        self.pos = start
+        self.dirnx = 1
+        self.dirny = 0
+        self.color = color
+ 
     def move(self, dirnx, dirny):
         pass
     def draw(self,surface, eyes=False):
@@ -23,7 +34,7 @@ class snake(object):
 
     def move(self):
         for event in pygame.event.get():
-            if event.type == pygame.WUIT:
+            if event.type == pygame.QUIT:
                 pygame.quit()
             
             keys = pygame.key.get_pressed()
@@ -33,25 +44,44 @@ class snake(object):
                     self.dirnx = -1
                     self.dirny = 0
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-                if keys[pygame.K_RIGHT]:
+                elif keys[pygame.K_RIGHT]:
                     self.dirnx = 1
                     self.dirny = 0
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-                if keys[pygame.K_UP]:
+                elif keys[pygame.K_UP]:
                     self.dirnx = 0
                     self.dirny = -1
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-                if keys[pygame.K_DOWN]:
+                elif keys[pygame.K_DOWN]:
                     self.dirnx = 0
                     self.dirny = 1
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+        
+        for i, c in enumerate(self.body):
+            p = c.pos[:]
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0], turn[1])
+                if i == len(self.body)-1:
+                    self.turns.pop(p)
+            else:
+                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
+                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
+                else: c.move(c.dirnx,c.dirny)
+
 
     def reset(self, pos):
         pass
     def addCube(self):
         pass
     def draw(self, surface):
-        pass
+        for i, c in enumerate(self.body):
+            if i == 0:
+                c.draw(surface, True)
+            else:
+                c.draw(surface)
 
 def drawGrid(w, rows, surface):
     sizeBtwn = w // rows
