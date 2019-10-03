838f732d0c9f96feb5d6eb75bf4785c73b6c182f
Robert Sohlberg <45578298+sohlbert@users.noreply.github.com>
2019-10-02 20:37:32 -0500

    Add files via upload
    adding directional keys.

--- Snake.Game.py
+++ Snake.Game.py
@@ -18,9 +18,34 @@ class snake(object):
         self.color = color
         self.head = cube(pos)
         self.body.append(self.head)
+        self.dirnx = 0
+        self.dirny = 1
 
     def move(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.WUIT:
+                pygame.quit()
+            
+            keys = pygame.key.get_pressed()
+
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    self.dirnx = -1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                if keys[pygame.K_RIGHT]:
+                    self.dirnx = 1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                if keys[pygame.K_UP]:
+                    self.dirnx = 0
+                    self.dirny = -1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                if keys[pygame.K_DOWN]:
+                    self.dirnx = 0
+                    self.dirny = 1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
     def reset(self, pos):
         pass
     def addCube(self):
